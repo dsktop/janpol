13fcb3a8f350b88f5520d1f8edc5dca0975433b6
pelucaa <107814253+pelucaa@users.noreply.github.com>
2022-07-11 19:42:36 -0500

    ta bien
    u

---
+++ ejercicio3JPGS.py
@@ -0,0 +1,27 @@
+def nem(nem)
+for i in num
+a=[]
+a.list(azul)
+
+azul={"Atleta1":(12,15,16)
+            ,"Atleta2":(18,15,16)
+           , "Atleta3":(19,18,17)
+            ,"Atleta4":(11,16,18)
+           , "Atleta5":(14,17,19)}
+a.sort(a)
+
+print("fin del programa ")
+
+
+
+
+
+"""dicc={"Atleta1":(12,15,16),"Atleta2":(18,15,16),"Atleta3":(19,18,17),"Atleta4":(11,16,18),"Atleta5":(14,17,19)}
+
+print("Atleta4 --> marca: ", dicc["Atleta4"][0])
+print("Atleta4 --> marca: ", dicc["Atleta1"][0])
+print("Atleta4 --> marca: ", dicc["Atleta5"][0])
+print("Atleta4 --> marca: ", dicc["Atleta2"][1])
+print("Atleta4 --> marca: ", dicc["Atleta3"][2])"""
+
+#por si sirve de algo para los puntitos :)
